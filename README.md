3021bdd274a79a14a05707010ccbe8ea42e363b4
huanshenyi <txy1226052>
2019-12-02 16:54:51 +0900

    ドキュメント追加

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 ```text
 pip install  locust
 ```
+
+実行例
+```text
+locust -f first-test.py --host=https://www.imooc.com/
+```
+-f :実行ファイルを選択
+--host:使用するurlを指定
+
+web画面は開く
+`127.0.0.1:8098`
+
+- Number of users to simulate  模擬するユーザーの数
+- Hatch rate 秒ごとに起動するユーザーの数
+
+
+UIなしで実行する
+```text
+locust -f first-test.py --host=https://www.imooc.com/ --no-web -c 10 -r 2 -t 1m
+```
+- -f テストスクリプト選択
+- --host:指定被测试应用的URL的地址
+- --no-web：UIなしで実行 
+- -c：模擬ユーザーの数
+- -r：毎秒ごとに立ち上げるユーザー
+- -t：テストの実行時間
--- first-test.py
+++ first-test.py
@@ -1,7 +1,7 @@
 from locust import HttpLocust, TaskSet, task
 
 
-# TestCase
+# テストを定義する
 class ImoocIndex(TaskSet):
     '''
     the VUser' behavior
@@ -12,7 +12,7 @@ class ImoocIndex(TaskSet):
 
 
 class WebsiteUser(HttpLocust):
-    task_set = ImoocIndex #将测试用例加入到测试任务套件中
-    min_wait = 1 # 每个请求间最小等待时间 リクエストの最小待ち間隔
-    max_wait = 5 # 每个请求间最大等待时间
+    task_set = ImoocIndex # テストケースをテストグループへ追加
+    min_wait = 1 # リクエストの最小待ち間隔
+    max_wait = 5 # リクエストの待ち最大間隔
     host = 'https://www.imooc.com/'
